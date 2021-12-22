86dc4b1a618d62f19586435dab0d17c1117f4cc3
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2021-12-22 21:06:38 +0300

    Add info_get_peers method

--- src/casper_client.rb
+++ src/casper_client.rb
@@ -3,12 +3,19 @@ require 'jimson'
 require 'json'
 
 class CasperClient
-	attr_accessor :ip_address, :port, :node_id 
-	
-	def initialize(ip_address = "", port = 0)
-		@ip_address = ip_address
-		@port = port
-	end
+  attr_accessor :ip_address, :port, :url, :node_id 
+  
+  def initialize(ip_address = "", port = 0)
+    @ip_address = ip_address
+    @port = port
+    @url = ""
+  end
+
+  def info_get_peers
+
+  end
 
 end
 
+obj = CasperClient.new
+obj.info_get_peers
