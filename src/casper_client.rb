92edfe8d6d954ade7a611f09614e25b8556b370f
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2021-12-23 05:23:06 +0300

    Add set_ipaddress_and_port_number

--- src/casper_client.rb
+++ src/casper_client.rb
@@ -3,23 +3,32 @@ require 'jimson'
 require 'json'
 
 class CasperClient
-  attr_accessor :ip_address, :port, :url, :node_id 
-  
+  attr_accessor :ip_address, :port, :url, :state_root_hash
+  @@test_url = "http://185.246.84.43:7777/rpc" 
+
   def initialize(ip_address = "", port = 0)
     @ip_address = ip_address
     @port = port
     @url = ""
+    @state_root_hash = ""
   end
 
-  def info_get_peers
+
+  def set_ipaddress_and_port_number
     puts "Enter IP Address:"
-    @ip_address = gets.chomp    # Example: 185.246.84.43
+    @ip_address = gets.chomp    # Input example: 185.246.84.43
     
-    puts "Enter Port Number:"   # Example: 7777
+    puts "Enter Port Number:"   # Input example: 7777
     @port = gets.chomp.to_i
     
     self.url = "http://" + self.ip_address + ":" + self.port.to_s + "/rpc"
-    
+  end
+
+  # Return a hash that include peers.
+  # Peer {node_id: string; address: string; }
+  def info_get_peers
+    set_ipaddress_and_port_number
+
     client = Jimson::Client.new(self.url)
     result = client.info_get_peers
     # puts result
@@ -28,4 +37,9 @@ class CasperClient
 end
 
 obj = CasperClient.new
-obj.info_get_peers
+
+# Test chain_get_state_root_hash
+puts obj.info_get_peers
+
+
+
