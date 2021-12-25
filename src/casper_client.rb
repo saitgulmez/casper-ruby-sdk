9c7c4f05df2f8e65f8621ea6c86f057844c8534e
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2021-12-25 22:40:56 +0300

    Change default value of port from 0 to 7777 and remove unnecessary initialization done for @url variable

--- src/casper_client.rb
+++ src/casper_client.rb
@@ -6,10 +6,17 @@ class CasperClient
   attr_accessor :ip_address, :port, :url, :state_root_hash
   @@test_url = "http://185.246.84.43:7777/rpc" 
 
-  def initialize(ip_address = "", port = 0)
+  def initialize(ip_address = "", port = 7777)
     @ip_address = ip_address
     @port = port
-    @url = ""
+
+    if @ip_address == ""
+      puts "Enter IP Address:"
+      @ip_address = gets.chomp
+    end
+
+    @url = "http://" + self.ip_address + ":" + self.port.to_s + "/rpc"
+
     @state_root_hash = ""
   end
 
@@ -20,9 +27,10 @@ class CasperClient
     puts "Enter Port Number:"   
     @port = gets.chomp.to_i
     self.url = "http://" + self.ip_address + ":" + self.port.to_s + "/rpc"
+    self.url = "http://" + self.ip_address + ":" + "7777/rpc"
   end
 
-  
+
   def set_ip_and_port(ip, port)
     @ip = ip
     @port = port
@@ -30,33 +38,54 @@ class CasperClient
 
   # Return peers.
   def info_get_peers
-    set_ipaddress_and_port_number
+    # set_ipaddress_and_port_number
     client = Jimson::Client.new(self.url)
     result = client.info_get_peers
   end
 
   # Return state_root_hash value
   def chain_get_StateRootHash
-    # Uncomment below line for any ip address that is available in the network
-    # self.info_get_peers
-    # client = Jimson::Client.new(self.url)
-
-    client = Jimson::Client.new(@@test_url)
+    client = Jimson::Client.new(self.url)
     result = client.chain_get_state_root_hash
     @state_root_hash = result["state_root_hash"]
   end
 
+  def info_get_deploy
+    client = Jimson::Client.new(@@test_url)
+    result = client.info_get_deploy
+  end
 
 
+  def state_get_AuctionInfo
+    client = Jimson::Client.new(@@test_url)
+    result = client.state_get_auction_info
+    result
+  end
+ 
 
 end
 
-# obj = CasperClient.new
+# Create an instance
+client1 = CasperClient.new
+client2 = CasperClient.new("164.68.97.38")
+# client3 = CasperClient.new("78.31.71.19")
 
-# # Test chain_get_state_root_hash
-# puts obj.info_get_peers
+# Test info_get_peers
+puts client1.info_get_peers
+# puts client2.info_get_peers
+# puts client3.info_get_peers
 
-# # Test chain_get_StateRootHash
-# puts obj.chain_get_StateRootHash
+# Test chain_get_StateRootHash
+# puts client1.chain_get_StateRootHash
+# puts client2.chain_get_StateRootHash
+# puts client3.chain_get_StateRootHash
 
+# Test info_get_deploy
+# puts client1.info_get_deploy
+# puts client2.info_get_deploy
+# puts client3.info_get_deploy
 
+# Test state_get_AuctionInfo
+# puts client1.state_get_AuctionInfo
+# puts client2.state_get_AuctionInfo
+# puts client3.state_get_AuctionInfo
