e041ae997e6a5d0e4b80aa09b105da19b42a8ec1
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-01-18 05:40:35 +0300

    Add era_summary.rb

---
+++ lib/entity/era_summary.rb
@@ -0,0 +1,38 @@
+
+module Casper
+  class EraSummary
+
+    def initialize(block_hash_, era_id_, stored_value_, state_root_hash_, merkle_proof_)
+      @block_hash = block_hash_
+      @era_id = era_id_
+      @stored_value = stored_value_
+      @state_root_hash = state_root_hash_
+      @merkle_proof = merkle_proof_
+    end
+
+    # @return [String] block_hash
+    def get_block_hash
+      @block_hash
+    end
+
+    # @return [Integer] era_id
+    def get_era_id
+      @era_id
+    end
+
+    # @return [StoredValue] StoredValue
+    def get_stored_value
+      @stored_value
+    end
+
+    # @return [String] state_root_hash
+    def get_state_root_hash
+      @state_root_hash
+    end
+
+    # @return [String] merkle_proof
+    def get_merkle_proof
+      @merkle_proof
+    end
+  end
+end
