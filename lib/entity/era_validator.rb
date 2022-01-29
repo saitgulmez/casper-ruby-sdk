5fc05685b0183a31788123386a6ee986b9a92593
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-01-29 04:51:29 +0300

    Add validator_weight.rb and era_validator.rb files

---
+++ lib/entity/era_validator.rb
@@ -0,0 +1,23 @@
+module Casper
+  module Entity
+    class EraValidator
+
+      # @param [Integer] era_id
+      # @param [Array<ValidatorWeight>] validator_weights
+      def initialize(era_id, validator_weights)
+        @era_id = era_id
+        @validator_weights = validator_weights
+      end
+
+      # @return [Integer] era_id
+      def get_era_id
+        @era_id
+      end
+
+      # @return [Array<ValidatorWeight>] validator_weights
+      def get_validator_weights
+        @validator_weights
+      end
+    end
+  end
+end
---
+++ lib/entity/validator_weight.rb
@@ -0,0 +1,23 @@
+module Casper
+  module Entity
+    class ValidatorWeight
+
+      # @param [String] public_key
+      # @param [String] weight
+      def initialize(public_key, weight)
+        @public_key = public_key
+        @weight = weight
+      end
+
+      # @return [String] public_key
+      def get_public_key
+        @public_key
+      end
+
+      # @return [String] weight
+      def get_weight
+        @weight
+      end
+    end
+  end
+end
