4347e90957bd2b0a4e85b7e9c6186020a173f56c
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-04-15 16:59:42 +0300

    Add constants.rb file

---
+++ lib/types/constants.rb
@@ -0,0 +1,26 @@
+
+BOOL_ID = 'Bool';
+KEY_ID = 'Key';
+PUBLIC_KEY_ID = 'PublicKey';
+STRING_ID = 'String';
+UREF_ID = 'URef';
+UNIT_ID = 'Unit';
+I32_ID = 'I32';
+I64_ID = 'I64';
+U8_ID = 'U8';
+U32_ID = 'U32';
+U64_ID = 'U64';
+U128_ID = 'U128';
+U256_ID = 'U256';
+U512_ID = 'U512';
+
+BYTE_ARRAY_ID = 'ByteArray';
+LIST_ID = 'List';
+MAP_ID = 'Map';
+OPTION_ID = 'Option';
+RESULT_ID = 'Result';
+TUPLE1_ID = 'Tuple1';
+TUPLE2_ID = 'Tuple2';
+TUPLE3_ID = 'Tuple3';
+
+ACCOUNT_HASH_ID = 'AccountHash';
