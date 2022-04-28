60d7b7692fc8db530639a88cddddadcc0f0c18a7
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-04-29 00:09:52 +0300

    Add CLI32BytesParser module

--- lib/serialization/cl_value_bytes_parsers.rb
+++ lib/serialization/cl_value_bytes_parsers.rb
@@ -4,7 +4,6 @@ module CLValueBytesParsers
     def from_bytes(raw_bytes)
       first_4_bytes = raw_bytes[0..7]
       string_length = [first_4_bytes].pack("H*").unpack("l").first
-      puts string_length
       rest = raw_bytes[8..raw_bytes.length]
       len2 = (rest.length) / 2
       if string_length == len2
@@ -15,13 +14,25 @@ module CLValueBytesParsers
     end
 
     def to_bytes(value)
-
+      len = value.length
+      hex_of_value = value.unpack("H*")
+      # hex_of_len = [len].
     end
 
     def to_json 
     end
 
     def from_json
+    end
+  end
+
+
+  module CLI32BytesParser
+    def from_bytes
+    end
+
+    def to_bytes(value)
+      [value].pack("L<*").unpack("C*")
     end
   end
   
