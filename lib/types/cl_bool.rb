24211c8fdf89bb05ca99248dc9b9cf89b1eab740
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-05-01 01:29:55 +0300

    Fix method

--- lib/types/cl_bool.rb
+++ lib/types/cl_bool.rb
@@ -1,5 +1,6 @@
 require_relative './cl_bool_type.rb'
 require_relative './cl_value.rb'
+require_relative '../serialization/cl_value_bytes_parsers.rb'
 
 
 module Boolean; end
@@ -16,6 +17,7 @@ class CLBool < CLValue
 
   def get_cl_type
     @cl_bool_type = CLBoolType.new(true)
+    @cl_bool_type.to_string
   end
 
   def get_value
