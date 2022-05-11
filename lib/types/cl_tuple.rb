ea3e34d9a14fd90e3d18b3d8b4079c19f55dc243
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-05-11 03:36:01 +0300

    Add CLTuple1, CLTuple2, CLTuple3 classes and some methods to cl_tuple.rb

--- lib/types/cl_tuple.rb
+++ lib/types/cl_tuple.rb
@@ -1,54 +1,110 @@
 require_relative './cl_tuple_type.rb'
 require_relative './cl_value.rb'
+require_relative './error.rb'
 require_relative '../serialization/cl_value_bytes_parsers.rb'
-require_relative './cl_bool.rb'
 
 
 
 class CLTuple < CLValue  
   # include CLValueBytesParsers::CLTupleBytesParser
- 
-  def initialize(size, array)
+  attr_accessor :size, :data
+  def initialize(size, data)
     super()
-    if array.size > size
+    @size = 0
+    @data = []
+    if data.size > size
       raise "Too many elements!" 
     end
-    if array.all? { |item| item.is_a?(CLValue)}
+    if data.all? { |item| item.is_a?(CLValue)}
       @size = size
-      @array = array
+      @data = data
     else
-      raise "Invalid data type(s) provided."
+      Error.new("Invalid data type(s) provided.")
     end
   end
 
   def get_cl_type
-    if @size == 1 && @array.size == 1
+    if @size == 1 && @data.size == 1
       @cl_type = CLTuple1Type.new
       @cl_type.to_string
-    elsif @size == 2 && @array.size == 2
+    elsif @size == 2 && @data.size == 2
       @cl_type = CLTuple2Type.new
       @cl_type.to_string
-    elsif @size == 3 && @array.size == 3
+    elsif @size == 3 && @data.size == 3
       @cl_type = CLTuple3Type.new
       @cl_type.to_string
     else
-      raise "Invalid Type"
+      Error.new("Invalid Type")
     end
   end
 
-  def get_data
-    @array  
-  end
-
-  def get_item(index)
-    if index >= @size
-      raise "Tuple index out of bounds."
-    end
-    @array[index]
+  def get_value
+    @data  
   end
 
   def get_size
     @size
   end
 
+  def get(index)
+    if index >= @size
+      Error.new("Tuple index out of bounds.")
+    else
+      @data[index]
+    end
+  end
+
+  def set(index, item) 
+    if index >= @size
+      Error.new("Tuple index out of bounds.")
+    else
+      data[index] = item
+    end
+  end
+
+  def push(item)
+    if @data.size < @size
+      @data.push(item)
+    else
+      Error.new("No more space in this tuple!")
+    end
+  end
+
+end
+
+
+class CLTuple1 < CLTuple
+  
+  def initialize(data)
+    super(1, data)
+  end
+
+  def get_cl_type
+    @cl_tuple_type = CLTuple1Type.new
+    @cl_tuple_type.to_string
+  end
+end
+
+class CLTuple2 < CLTuple
+  
+  def initialize(data)
+    super(2, data)
+  end
+
+  def get_cl_type
+    @cl_tuple_type = CLTuple2Type.new
+    @cl_tuple_type.to_string
+  end
+end
+
+class CLTuple3 < CLTuple
+  
+  def initialize(data)
+    super(3, data)
+  end
+
+  def get_cl_type
+    @cl_tuple_type = CLTuple3Type.new
+    @cl_tuple_type.to_string
+  end
 end
