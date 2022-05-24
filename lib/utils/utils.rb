7552689232bb0be808a7651c7763e614df127239
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-05-24 22:27:48 +0300

    Add ByteUtil module

---
+++ lib/utils/utils.rb
@@ -0,0 +1,48 @@
+module Utils
+  module ByteUtil
+    extend self
+
+    # @param [String] string
+    # @return [Array] byte_array
+    def string_to_byte_array(str)
+      
+    end
+
+
+    # @param [Array] byte_array
+    # @return [String] string
+    def byte_array_to_string(byte_array)
+
+    end 
+
+    # @param [Array] byte_array
+    # @return [String] string
+    def byte_array_to_hex(byte_array)
+
+    end
+
+    # @param [String] hex_string
+    # @return [Array] byte_array
+    def hex_to_byte_array(hex_string)
+
+    end
+
+    # @param [String] string
+    # @return [String] hex_string
+    def string_to_hex(string)
+
+    end
+
+    # @param [String] hex_string
+    # @return [String] string
+    def hex_to_string(hex_string)
+
+    end
+
+    # @param [String] string
+    # @param [Array<int>] bytes
+    def string_to_bytes_u32(string)
+
+    end
+  end
+end
