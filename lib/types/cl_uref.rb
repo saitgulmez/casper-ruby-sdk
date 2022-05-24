5164262a04712cef843ee1e52e85ae345f056ca2
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-05-24 22:06:56 +0300

    Add CLURefType and CLURef classes

---
+++ lib/types/cl_uref.rb
@@ -0,0 +1,47 @@
+require_relative './cl_uref_type.rb'
+require_relative './cl_value.rb'
+require_relative '../serialization/cl_value_bytes_parsers.rb'
+
+#  Access Rights
+# https://docs.casperlabs.io/design/serialization-standard/#clvalue-uref
+
+
+class CLURef < CLValue  
+  include CLValueBytesParsers::CLURefBytesParser
+  
+  # @param [Array<Integer>]
+  # value is an uint8array
+  def initialize(value, access_rights)
+    super()
+    raise ArgumentError.new("The length of URefAddr should be 32") unless value.length == 32
+    raise ArgumentError.new("Unsupported AccessRights") unless AccessRights.has_value?(access_rights)
+    # if value.length != 32
+    #   begin
+    #     raise Error.new('The length of URefAddr should be 32')
+    #   rescue StandardError => e
+    #     puts e
+    #   end
+    # end
+    # if !(AccessRights.has_value?(access_rights))
+    #   raise Error.new('Unsupported AccessRights')
+    # end
+    @value =  value
+    @access_rights = access_rights
+
+  end
+
+  def get_cl_type
+    @cl_type = CLURefType.new
+    @cl_type.to_string
+  end
+
+  def get_value
+    @value  
+  end
+
+  def get_size
+    @value.size
+  end
+
+end
+
---
+++ lib/types/cl_uref_type.rb
@@ -0,0 +1,42 @@
+require_relative './cl_type.rb'
+require_relative './constants.rb'
+
+
+
+class CLURefType < CLType
+  def initialize(value = nil)  
+    super(value)
+    @value = value
+  end
+
+  def to_string
+    TAGS.key(12).to_s
+  end
+
+  def get_value 
+    @value
+  end
+
+  def to_json 
+    to_string
+  end
+end
+
+AccessRights = {
+  # No permissions
+  None: 0b00000000,
+  # Permission to read the value under the associated [[URef]].
+  READ: 0b00000001,
+  # Permission to write a value under the associated [[URef]].
+  WRITE: 0b00000010,
+  # Permission to read or write the value under the associated [[URef]].
+  READ_WRITE: 0b00000011,
+  # Permission to add to the value under the associated [[URef]].
+  ADD: 0b00000100,
+  # # Permission to read or add to the value under the associated [[URef]].
+  READ_ADD: 0b00000101,
+  # # Permission to add to, or write the value under the associated [[URef]].
+  ADD_WRITE: 0b00000110,
+  # # Permission to read, add to, or write the value under the associated [[URef]].
+  READ_ADD_WRITE: 0b00000111
+}
