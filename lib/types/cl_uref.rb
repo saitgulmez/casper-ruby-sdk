5f7b44e23fd48720705f38d1076c7d31036f7c2d
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-05-25 19:31:50 +0300

    Update CLURef constructor and add some methods

--- lib/types/cl_uref.rb
+++ lib/types/cl_uref.rb
@@ -8,13 +8,28 @@ require_relative '../serialization/cl_value_bytes_parsers.rb'
 
 class CLURef < CLValue  
   include CLValueBytesParsers::CLURefBytesParser
-  
+  attr_accessor :err
   # @param [Array<Integer>]
   # value is an uint8array
   def initialize(value, access_rights)
     super()
-    raise ArgumentError.new("The length of URefAddr should be 32") unless value.length == 32
-    raise ArgumentError.new("Unsupported AccessRights") unless AccessRights.has_value?(access_rights)
+    # raise ArgumentError.new('The length of URefAddr should be 32') unless value.length == 32
+    # rescue ArgumentError => e
+    #   p e.message
+    begin
+      raise ArgumentError.new('The length of URefAddr should be 32') unless value.length == 32
+    rescue ArgumentError => e  
+      e.message
+    end
+    begin
+      raise ArgumentError.new('Unsupported AccessRights') unless AccessRights.has_value?(access_rights)
+    rescue ArgumentError => e  
+      e.message
+    end
+    # if value.length != 32
+    #   raise ArgumentError.new('The length of URefAddr should be 32')
+    # end
+    # raise ArgumentError.new("Unsupported AccessRights") unless AccessRights.has_value?(access_rights)
     # if value.length != 32
     #   begin
     #     raise Error.new('The length of URefAddr should be 32')
@@ -36,7 +51,11 @@ class CLURef < CLValue
   end
 
   def get_value
-    @value  
+    @value 
+  end
+
+  def get_access_rights
+    @access_rights
   end
 
   def get_size
