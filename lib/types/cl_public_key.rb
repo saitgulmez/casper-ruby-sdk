4a6c902169e45d7d40561c7796c0097bce955f93
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-06-09 05:52:56 +0300

    Implement to_account_hash_byte_array and to_account_hash_hex methods for CLPublicKey

--- lib/types/cl_public_key.rb
+++ lib/types/cl_public_key.rb
@@ -3,7 +3,8 @@ require_relative './cl_value.rb'
 require_relative '../serialization/cl_value_bytes_parsers.rb'
 require_relative '../utils/base_16.rb'
 require_relative '../utils/hex_utils.rb'
-
+require_relative '../utils/hash_utils.rb'
+require 'blake2'
 =begin
 PublicKey serializes as a single byte tag representing the algorithm followed by 32 bytes of the PublicKey itself:
 If the PublicKey is an Ed25519 key, the single tag byte is 1 followed by the individual bytes of the serialized key.
@@ -24,9 +25,11 @@ CLPublicKeyTag = {
 }
 
 
+
+
 class CLPublicKey < CLValue  
   include CLValueBytesParsers::CLPublicKeyBytesParser
-  
+  include Utils::HashUtils
   # @param [Array<Integer>] raw_public_key
   # @param [Integer] tag
   def initialize(raw_public_key, tag)
@@ -62,6 +65,22 @@ class CLPublicKey < CLValue
     @raw_public_key  
   end
 
+  def get_cl_public_key_tag
+    if @tag == CLPublicKeyTag[:ED25519] || @tag == SignatureAlgorithm[:Ed25519]
+      CLPublicKeyTag[:ED25519]
+    else
+      CLPublicKeyTag[:SECP256K1] 
+    end
+  end
+
+  def get_signature_algorithm
+    if @tag == CLPublicKeyTag[:ED25519] || @tag == SignatureAlgorithm[:Ed25519]
+      SignatureAlgorithm[:Ed25519]
+    else
+      SignatureAlgorithm[:Secp256K1]
+    end
+  end
+
   def ed25519?
     @tag == CLPublicKeyTag[:ED25519]
   end
@@ -70,17 +89,34 @@ class CLPublicKey < CLValue
     @tag == CLPublicKeyTag[:SECP256K1]
   end
 
-  def to_account_hash
+  # @return [Array<Integer>] 
+  def to_account_hash_byte_array
+    key_name = CLPublicKeyTag.key(@tag).to_s
+    prefix = key_name.downcase.unpack("C*") + [0]
+    bytes = prefix + @raw_public_key
+    result_array = Utils::HashUtils.byte_hash(bytes)
+    @raw_public_key.length == 0 ? [] : result_array
   end
 
-  def to_account_hash_str
+  # @return [String] hex
+  def to_account_hash_hex
+    bytes = to_account_hash_byte_array
+    hex = Utils::HashUtils.account_hash_from_byte_to_hex(bytes)
   end
 
   def from_ed25519(public_key)
     CLPublicKey.new(public_key, CLPublicKeyTag[:ED25519])
   end
 
+  def self.from_ed25519(public_key)
+    CLPublicKey.new(public_key, CLPublicKeyTag[:ED25519])
+  end
+
   def from_secp256k1(public_key)
+    CLPublicKey.new(public_key, CLPublicKeyTag[:SECP256K1])
+  end
+
+  def self.from_secp256k1(public_key)
     CLPublicKey.new(public_key, CLPublicKeyTag[:SECP256K1])
   end
 
@@ -91,10 +127,14 @@ class CLPublicKey < CLValue
   
   def from_hex(public_key_hex)
     raise ArgumentError.new("Invalid public key") unless Utils::HexUtils.valid_public_key_format?(public_key_hex)
-    public_key_hex_bytes = Utils.Base16.decode(public_key_hex)
+    public_key_hex_bytes = Utils::Base16.decode16(public_key_hex)
+    CLPublicKey.new(public_key_hex_bytes.drop(1), public_key_hex_bytes[0])
+  end
+
+  def self.from_hex(public_key_hex)
+    raise ArgumentError.new("Invalid public key") unless Utils::HexUtils.valid_public_key_format?(public_key_hex)
+    public_key_hex_bytes = Utils::Base16.decode16(public_key_hex)
     CLPublicKey.new(public_key_hex_bytes.drop(1), public_key_hex_bytes[0])
   end
 
 end
-
-# type = CLPublicKey.new("asds", 1)
