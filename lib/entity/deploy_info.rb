8903c869cd76a49f2a9a249d8aa39a41fe1e2232
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-06-18 03:36:38 +0300

    Add DeployInfo class

---
+++ lib/entity/deploy_info.rb
@@ -0,0 +1,44 @@
+module Casper
+  module Entity
+    class DeployInfo
+    
+      # @param [String] hash
+      # @param [Array] transfers
+      # @param [String] from
+      # @param [String] source
+      # @param [String] gas
+      def initialize(hash, transfers, from, source, gas)
+        @hash = hash
+        @transfers = transfers
+        @from = from
+        @source = source
+        @gas = gas
+      end
+
+      # @return [String] hash
+      def get_hash
+        @hash 
+      end
+
+      # @return [Array] transfers
+      def get_transfers
+        @transfers 
+      end
+
+      # @return [String] from
+      def get_from 
+        @from  
+      end
+
+      # @return [String] source
+      def get_source 
+        @source  
+      end
+
+      # @return [String] gas
+      def get_gas
+        @gas  
+      end
+    end
+  end
+end
