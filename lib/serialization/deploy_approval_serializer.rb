959a97f0bd68f9d3f253d1e56b4935bfd9e37061
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-07-01 23:43:30 +0300

    Implement DeployApprovalSerializer

---
+++ lib/serialization/deploy_approval_serializer.rb
@@ -0,0 +1,15 @@
+require_relative '../entity/deploy_approval.rb'
+require_relative '../serialization/cl_value_bytes_parsers.rb'
+require_relative '../utils/time_utils.rb'
+require_relative '../utils/byte_utils.rb'
+
+class DeployApprovalSerializer
+  def to_bytes(deploy_approval)
+    @approval_serializer = ""
+    @signer = deploy_approval.get_signer
+    @approval_serializer << @signer
+    
+    @signature = deploy_approval.get_signature
+    @approval_serializer << @signature
+  end
+end
