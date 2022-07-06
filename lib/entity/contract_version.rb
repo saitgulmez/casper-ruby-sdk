60fdef41755464241173e17d98448f365624d8e5
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-07-06 15:22:26 +0300

    Add ContractVersion

---
+++ lib/entity/contract_version.rb
@@ -0,0 +1,32 @@
+module Casper
+  module Entity
+
+    # The version of the contract.
+    class ContractVersion
+      
+      # @param [Integer] protocol_version_major
+      # @param [Integer] contract_version 
+      # @param [String] contract_hash
+      def initialize(protocol_version_major, contract_version, contract_hash)
+        @protocol_version_major = protocol_version_major
+        @contract_version = contract_version
+        @contract_hash = contract_hash
+      end
+
+      # @return [Integer] protocol_version_major
+      def get_protocol_version_major
+        @protocol_version_major
+      end
+
+      # @return [Integer] contract_version 
+      def get_contract_version
+        @contract_version
+      end
+      
+      # @return [String] contract_hash
+      def get_contract_hash
+        @contract_hash
+      end
+
+    end    
+  end
