9ce86d1c5f6ff656203ca7a1e23352cc8cf924e2
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-07-06 11:41:00 +0300

    Add Contract

---
+++ lib/entity/contract.rb
@@ -0,0 +1,47 @@
+module Casper
+  module Entity
+
+    # Contract entity class
+    class Contract 
+      
+      # @param [String] contract_package_hash
+      # @param [String] contract_wasm_hash
+      # @param [Array] entry_points
+      # @param [Array] named_keys
+      # @param [String] protocol_version
+      def initialize(contract_package_hash, contract_wasm_hash, entry_points, named_keys, protocol_version)
+        @contract_package_hash = contract_package_hash
+        @contract_wasm_hash = contract_wasm_hash
+        @entry_points = entry_points
+        @named_keys = named_keys
+        @protocol_version = protocol_version
+      end
+
+      # @return [String] contract_package_hash
+      def get_contract_package_hash
+        @contract_package_hash
+      end
+
+      # @return [String] contract_wasm_hash
+      def get_contract_wasm_hash
+        @contract_wasm_hash
+      end
+
+      # @return [Array] entry_points
+      def get_entry_points
+        @entry_points
+      end
+
+      # @return [Array] named_keys
+      def get_named_keys
+        @named_keys
+      end
+
+      # @return [String] protocol_version
+      def get_protocol_version
+        @protocol_version
+      end
+
+    end
+  end
+end
