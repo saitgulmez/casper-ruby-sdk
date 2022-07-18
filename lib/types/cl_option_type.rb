7cfa3e6419a679448fdd13f7c0f0df540af3778e
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-07-18 23:28:38 +0300

    Update CLOptionType

--- lib/types/cl_option_type.rb
+++ lib/types/cl_option_type.rb
@@ -1,22 +1,39 @@
 require_relative './cl_type.rb'
+require_relative './cl_bool.rb'
+require_relative './cl_bool_type.rb'
 require_relative './constants.rb'
 
+optionTags = {
+  NONE: 0,
+  SOME: 1
+}
 
 class CLOptionType < CLType
-  def initialize(value = nil)  
-    super(value)
-    @value = value
+  def initialize(inner_type = nil)  
+    super
+    @inner_type = inner_type
+    @tag = TAGS["Option"].to_s
   end
 
   def to_string
     TAGS.key(13).to_s
-  end
-
-  def get_value 
-    @value
+    if @inner_type.get_value == nil
+      TAGS.key(13).to_s + " (None)"
+    else
+      TAGS.key(13).to_s + " (#{@inner_type.to_string})"
+    end
   end
 
   def to_json 
     to_string
   end
+
+  def get_inner_type
+    @inner_type
+  end
+
+  def get_tag
+    @tag
+  end
+
 end
