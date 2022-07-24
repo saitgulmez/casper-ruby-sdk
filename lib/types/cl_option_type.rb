210b5e7e655f258d460d54d3572aa563d60665a1
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-07-24 21:43:30 +0300

    Implement to_json method for CLOptionType

--- lib/types/cl_option_type.rb
+++ lib/types/cl_option_type.rb
@@ -9,31 +9,64 @@ optionTags = {
 }
 
 class CLOptionType < CLType
-  def initialize(inner_type = nil)  
-    super
-    @inner_type = inner_type
-    @tag = TAGS["Option"].to_s
+
+  def initialize(inner)
+    @tag = TAGS[:Option]
+    @inner = inner
+    # p @inner.to_string
+    # p @inner.to_json
+    # p @inner.get_tag
   end
 
+  # @return [String]
   def to_string
-    TAGS.key(13).to_s
-    if @inner_type.get_value == nil
-      TAGS.key(13).to_s + " (None)"
-    else
-      TAGS.key(13).to_s + " (#{@inner_type.to_string})"
-    end
+    @inner == nil ? TAGS.key(13).to_s + " (None)" : TAGS.key(13).to_s + " (#{@inner.to_string})"
   end
 
-  def to_json 
-    to_string
+  def to_json
+    jsoned =  { "#{TAGS.key(13)}": @inner.to_string }.to_json
+    # h = JSON.parse(jsoned)
+    # h
   end
 
   def get_inner_type
-    @inner_type
+    @inner
   end
 
-  def get_tag
-    @tag
-  end
 
 end
+
+
+
+
+  # def initialize(inner_type = nil)  
+  #   super
+  #   @inner_type = inner_type
+  #   @tag = TAGS["Option"].to_s
+  # end
+
+  # def to_string
+  #   TAGS.key(13).to_s
+  #   if @inner_type == nil
+  #     TAGS.key(13).to_s + " (None)"
+  #   else
+  #     @inner_type.get_cl_tag
+  #     TAGS.key(13).to_s + " (#{@inner_type.to_string})"
+  #   end
+  # end
+
+  # def to_json 
+  #   to_string
+  # end
+
+  # def get_inner_type
+  #   @inner_type
+  # end
+
+  # def get_tag
+  #   @tag
+  # end
+
+  # def to_bytes
+
+  # end
