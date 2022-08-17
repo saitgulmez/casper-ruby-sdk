6512dcc3dfd92e4c2970fadb489c097a6b24c09c
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-08-17 22:55:42 +0300

    Add some methods for CLValueSerializer

--- lib/serialization/cl_value_serializer.rb
+++ lib/serialization/cl_value_serializer.rb
@@ -60,9 +60,38 @@ class CLValueSerializer
       serialized +=  helper(clvalue.get_value[0]) + [tag].pack("C*").unpack1("H*") + [tag1].pack("C*").unpack1("H*")
       # [18].pack("C*").unpack1("H*") + cl_type.get_data[0].to_bytes
     elsif type == "Tuple2"
-      [19].pack("C*").unpack1("H*") + cl_type.get_data[0].to_bytes + cl_type.get_data[1].to_bytes
+      clvalue1 = clvalue.get_value[0]
+      type1 = clvalue1.get_cl_type
+      value1 = clvalue1.get_value
+      tag1 = CLType::TAGS[type1.to_sym]
+      
+      clvalue2 = clvalue.get_value[1]
+      type2 = clvalue2.get_cl_type
+      value2 = clvalue2.get_value
+      tag2 = CLType::TAGS[type2.to_sym]
+      
+      len = only_length(clvalue.get_value[0]) + only_length(clvalue.get_value[1]) 
+      [len].pack("L<*").unpack1("H*") +  only_value(clvalue.get_value[0]) + only_value(clvalue.get_value[1]) +
+      [tag].pack("C*").unpack1("H*") + [tag1].pack("C*").unpack1("H*") + [tag2].pack("C*").unpack1("H*")
     elsif type == "Tuple3"
-      [20].pack("C*").unpack1("H*") + cl_type.get_data[0].to_bytes + cl_type.get_data[1].to_bytes + cl_type.get_data[2].to_bytes
+      clvalue1 = clvalue.get_value[0]
+      type1 = clvalue1.get_cl_type
+      value1 = clvalue1.get_value
+      tag1 = CLType::TAGS[type1.to_sym]
+      
+      clvalue2 = clvalue.get_value[1]
+      type2 = clvalue2.get_cl_type
+      value2 = clvalue2.get_value
+      tag2 = CLType::TAGS[type2.to_sym]      
+     
+      clvalue3 = clvalue.get_value[2]
+      type3 = clvalue3.get_cl_type
+      value3 = clvalue3.get_value
+      tag3 = CLType::TAGS[type3.to_sym]
+      
+      len = only_length(clvalue.get_value[0]) + only_length(clvalue.get_value[1]) + only_length(clvalue.get_value[2])  
+      [len].pack("L<*").unpack1("H*") +  only_value(clvalue.get_value[0]) + only_value(clvalue.get_value[1]) +  only_value(clvalue.get_value[2]) +
+      [tag].pack("C*").unpack1("H*") + [tag1].pack("C*").unpack1("H*") + [tag2].pack("C*").unpack1("H*") + [tag3].pack("C*").unpack1("H*")
     elsif type == "Any"
       [0].pack("L<*").unpack1("H*")
     elsif type == "PublicKey"
@@ -105,6 +134,95 @@ class CLValueSerializer
       uref = clvalue.get_value
       size = clvalue.to_bytes(uref).length/2
       [size].pack("L<*").unpack1("H*") + clvalue.to_bytes(uref)
+    elsif type == "Option"
+      [0].pack("L<*").unpack1("H*")
+    elsif type == "List"
+      [0].pack("L<*").unpack1("H*")
+    elsif type == "ByteArray"
+      [0].pack("L<*").unpack1("H*")
+    elsif type == "Result"
+      [0].pack("L<*").unpack1("H*")
+    elsif type == "Map"
+      [0].pack("L<*").unpack1("H*")
+    else
+      "Undefined"
+    end
+  end
+
+  def only_length(clvalue)
+    type = clvalue.get_cl_type
+    value = clvalue.get_value
+    if type == "Bool"
+      1
+    elsif type == "I32"
+      4
+    elsif type == "I64"
+      8
+    elsif type == "U8"
+      1
+    elsif type == "U32"
+      4
+    elsif type == "U64"
+      8
+    elsif type == "U128"
+      8
+    elsif type == "U256"
+      8
+    elsif type == "U512"
+      8
+    elsif type == "Unit"
+      8
+    elsif type == "String"
+      4 + value.length
+    elsif type == "Key"
+      32
+    elsif type == "URef"
+      uref = clvalue.get_value
+      size = clvalue.to_bytes(uref).length/2
+    elsif type == "Option"
+      8
+    elsif type == "List"
+      8
+    elsif type == "ByteArray"
+      8
+    elsif type == "Result"
+      8
+    elsif type == "Map"
+      8
+    else
+      0
+    end
+  end
+  def only_value(clvalue)
+    type = clvalue.get_cl_type
+    value = clvalue.get_value
+    if type == "Bool"
+      [value.to_i].pack("C*").unpack1("H*")
+    elsif type == "I32"
+      [value].pack("l<*").unpack1("H*")
+    elsif type == "I64"
+      [value].pack("q<*").unpack1("H*")
+    elsif type == "U8"
+      [value].pack("C*").unpack1("H*")
+    elsif type == "U32"
+      [value].pack("L<*").unpack1("H*")
+    elsif type == "U64"
+      [value].pack("Q<*").unpack1("H*")
+    elsif type == "U128"
+      [value].pack("Q<*").unpack1("H*")
+    elsif type == "U256"
+      [value].pack("Q<*").unpack1("H*")
+    elsif type == "U512"
+      [value].pack("Q<*").unpack1("H*")
+    elsif type == "Unit"
+      [value].pack("C*").unpack1("H*")
+    elsif type == "String"
+      CLValueBytesParsers::CLStringBytesParser.to_bytes(value)
+    elsif type == "Key"
+      [value].pack("C*").unpack1("H*")
+    elsif type == "URef"
+      uref = clvalue.get_value
+      clvalue.to_bytes(uref)
     elsif type == "Option"
       [0].pack("L<*").unpack1("H*")
     elsif type == "List"
