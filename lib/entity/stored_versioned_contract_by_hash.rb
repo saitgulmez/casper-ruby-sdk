a627d4489768bd566396aaf1dbd22f85d8314fd3
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-08-24 16:35:26 +0300

    Add StoredVersionedContractByHash entity

---
+++ lib/entity/stored_versioned_contract_by_hash.rb
@@ -0,0 +1,35 @@
+module Casper
+  module Entity
+    class StoredVersionedContractByHash < DeployExecutableItemInternal
+   
+      def initialize(hash, version, entry_point, args)
+        @tag = 3
+        @hash = hash
+        @version = version
+        @entry_point = entry_point
+        @args = args
+      end
+
+      def get_tag
+        @tag
+      end
+
+      def get_hash
+        @hash
+      end
+
+      def get_version
+        @version
+      end
+
+      def get_entry_point
+        @entry_point
+      end
+
+      def get_args
+        @args 
+      end
+
+    end
+  end
+end
