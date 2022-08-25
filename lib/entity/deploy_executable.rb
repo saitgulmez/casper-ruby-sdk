b02837c3940bba76ffed16e00788e4417e04c573
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-08-25 19:26:49 +0300

    Implement DeployExecutable partially

--- lib/entity/deploy_executable.rb
+++ lib/entity/deploy_executable.rb
@@ -1,8 +1,46 @@
+require_relative './deploy_named_argument.rb'
+require_relative './module_bytes.rb'
+require_relative './deploy_executable_transfer.rb'
+
 module Casper
   module Entity
     class DeployExecutable 
-      
+
       def initialize
+        @module_bytes = nil
+        @stored_contract_by_hash = nil
+        @stored_contract_by_name = nil
+        @stored_versioned_contract_by_hash = nil
+        @stored_versioned_contract_by_name = nil
+        @transfer = nil
+      end
+
+      def standard_payment(payment_amount)
+        @module_bytes = ModuleBytes.new("", [])
+        arg = DeployNamedArgument.new("amount", CLu512.new(payment_amount))
+        @module_bytes.set_arg(arg) # => Add arg and return args
+        @module_bytes
+      end
+
+      # @param [Integer] id
+      # @param [Integer] amount
+      # @param [CLURef | CLPublicKey] target
+      # @param [CLURef | CLPublicKey] source_purse
+      # @return [DeployExecutable] 
+      def new_transfer(id, amount, target, source_purse = nil)
+        if target.instance_of? CLURef
+          @target = target
+        elsif target.instance_of? CLPublicKey
+          @target = target.to_account_hash_byte_array
+        else
+          raise ArgumentError.new("Please specify target")
+        end
+        arg1 = DeployNamedArgument.new("amount", CLu512.new(amount))
+        arg2 = DeployNamedArgument.new("target", @target)
+        @transfer = DeployExecutableTransfer.new([])
+        @transfer.set_arg(arg1)
+        @transfer.set_arg(arg2)
+        @transfer
       end
     end
   end
