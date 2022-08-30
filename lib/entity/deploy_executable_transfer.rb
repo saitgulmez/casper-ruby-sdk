40c49bb55e7a98854dc45f4eb296407aaf37847c
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-08-30 23:11:53 +0300

    Implement to_bytes method for DeployExecutableTransfer

--- lib/entity/deploy_executable_transfer.rb
+++ lib/entity/deploy_executable_transfer.rb
@@ -1,3 +1,18 @@
+=begin
+"Transfer": {
+  "args": [
+    [
+      "amount",
+      {
+        "cl_type": "I32",
+        "bytes": "e8030000",
+        "parsed": 1000
+      }
+    ]
+  ]
+}
+
+=end     
 module Casper
   module Entity
     class DeployExecutableTransfer < DeployExecutableItemInternal
@@ -16,6 +31,18 @@ module Casper
         @args 
       end
 
+      def to_bytes
+        serializer = DeployNamedArgSerializer.new
+        num_of_args = @args.length
+        bytes = Utils::ByteUtils.to_u8(@tag) + Utils::ByteUtils.to_u32(num_of_args)
+
+        @args.each do |arg|
+          arg.each do |item|
+            bytes += serializer.to_bytes(item)
+          end
+        end
+        bytes
+      end
     end
   end
 end
