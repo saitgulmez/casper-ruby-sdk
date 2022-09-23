2316068a27116a194abcc10e6283e4b836f1b608
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-09-23 21:16:57 +0300

    Refactor BlockBody entity

--- lib/entity/block_body.rb
+++ lib/entity/block_body.rb
@@ -1,28 +1,31 @@
 module Casper
   module Entity
-    # BlockBody  
+    # BlockBody entity 
     class BlockBody
 
-      # @param [String] proposer
-      # @param [Array] deploy_hashes
-      # @param [Array] transfer_hashes
+      # @param [Hash] body
+      # @option [String] :proposer
+      # @option [Array] :deploy_hashes
+      # @option [Array] :transfer_hashes
       def initialize(body =  {})
         @proposer = body[:proposer]
         @deploy_hashes = body[:deploy_hashes]
         @transfer_hashes = body[:transfer_hashes]
       end
 
-      # @return [String] proposer
+      # @return [String] a hex-encoded cryptographic public key, 
+      #   including the algorithm tag prefix.
       def get_proposer
         @proposer
       end
 
-      # @return [Array] deploy_hashes
+      # @return [Array] hex-encoded Deploy hashes.
       def get_deploy_hashes
         @deploy_hashes
       end
 
-      # @return [Array] transfer_hashes
+      # @return [Array] a vector of hex-encoded hashes 
+      #   identifying Transfers included in this block.
       def get_transfer_hashes
         @transfer_hashes
       end
