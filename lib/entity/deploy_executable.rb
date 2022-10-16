0fcbe5ca6ef21f3eefd6dcd857deddafc895686b
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-10-16 23:18:31 +0300

    Fix in DeployExecutable

--- lib/entity/deploy_executable.rb
+++ lib/entity/deploy_executable.rb
@@ -1,26 +1,69 @@
 require_relative './deploy_named_argument.rb'
 require_relative './module_bytes.rb'
 require_relative './deploy_executable_transfer.rb'
-
+require_relative '../utils/helpers.rb'
+require_relative '../serialization/deploy_serializer.rb'
 module Casper
   module Entity
     # DeployExecutable
     class DeployExecutable 
+      include Utils::Helpers
       attr_accessor :module_bytes, :transfer, :stored_contract_by_hash, :stored_contract_by_name,
       :stored_versioned_contract_by_hash, :stored_versioned_contract_by_name
-      def initialize
-        @module_bytes = nil
+      
+      def initialize(h = {})
+        deploy_serializer = DeploySerializer.new()
+        temp_args = []
+        deploy_serializer = DeploySerializer.new
+        if h.keys[0] == :ModuleBytes
+          args = h[:ModuleBytes][:args]
+          args.each do |arg|
+            name1 = arg[0]
+            clvalue_hash = arg[1]
+            clvalue = deploy_serializer.build_cl_value(arg[1])
+        
+            temp_args << [Casper::Entity::DeployNamedArgument.new(name1, clvalue)]
+          end
+          @module_bytes = Casper::Entity::ModuleBytes.new(h[:ModuleBytes][:module_bytes], temp_args)
+        elsif h.keys[0] == :Transfer
+          temp_args = []
+          args = h[:Transfer][:args]
+
+          transfer = Casper::Entity::DeployExecutableTransfer.new(args)
+
+          args.each do |arg|
+            name1 = arg[0]
+            if name1 == "amount" || name1 == "target"
+              clvalue_hash = arg[1]
+              clvalue = deploy_serializer.build_cl_value(arg[1])
+              temp_args << [Casper::Entity::DeployNamedArgument.new(name1, clvalue)]
+            elsif name1 == "id"
+              bytes = arg[1][:bytes]
+              parsed = arg[1][:parsed]
+              h = arg[1][:cl_type]
+              key, value = h.first
+              cl_type = h.keys[0]
+              inner_type = value
+
+              inner_clvalue = Utils::Helpers.construct_inner_clvalue(inner_type, parsed)
+              clvalue = CLOption.new(inner_clvalue, inner_type)
+
+              temp_args << [Casper::Entity::DeployNamedArgument.new(name1, clvalue)]
+            end
+          end
+          @transfer = Casper::Entity::DeployExecutableTransfer.new(temp_args)
+        end
         @stored_contract_by_hash = nil
         @stored_contract_by_name = nil
         @stored_versioned_contract_by_hash = nil
         @stored_versioned_contract_by_name = nil
-        @transfer = nil
+         # @transfer = nil
       end
 
       def standard_payment(payment_amount)
         @module_bytes = ModuleBytes.new("", [])
         arg = DeployNamedArgument.new("amount", CLu512.new(payment_amount))
-        @module_bytes.set_arg(arg) # => Add arg and return args
+        @module_bytes.set_arg(arg)
         @module_bytes
       end
 
@@ -154,9 +197,10 @@ module Casper
         elsif stored_versioned_contract_by_hash?
           @stored_versioned_contract_by_hash.to_bytes
         elsif transfer?
-          @transfer.to_bytes
+          @transfer.to_bytes    
+        else
+          raise "failed to serialize ExecutableDeployItemJsonWrapper"
         end
-        raise "failed to serialize ExecutableDeployItemJsonWrapper"
       end
     end
   end
