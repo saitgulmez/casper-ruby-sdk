7f2c490bd6122c7212c93aca7436d9c23017ad9d
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-10-17 17:51:12 +0300

    Add some comments to ModuleBytes entity

--- lib/entity/module_bytes.rb
+++ lib/entity/module_bytes.rb
@@ -7,34 +7,39 @@ module Casper
 
       # @param [String] module_bytes
       # @param [Array<Array<DeployNamedArgument>>] args
-      def initialize(module_bytes=nil, args = [])
+      def initialize(module_bytes, args = [])
         @tag = 0
         @module_bytes = module_bytes
         @args = args
       end
 
+      # @return [Integer] the tag value
       def get_tag
         @tag
       end
       
+      # @return [String] the module bytes
       def get_module_bytes
         @module_bytes
       end
 
+      # @return [Array<DeployNamedArgument>] an array of DeployNamedArgument objects
       def get_args
         @args
       end
 
+      # @param [DeployNamedArgument] 
+      # @return [Array<DeployNamedArgument>] an array of DeployNamedArgument objects
       def set_arg(deploy_named_arg)
         @args << [deploy_named_arg]
       end
 
+      # @return [Array<Integer>] the byte serialization of ModuleByte object
       def to_bytes
         bytes = ""
         serializer = DeployNamedArgSerializer.new
         num_of_args = @args.length
         bytes += Utils::ByteUtils.to_u8(@tag)
-
         if @module_bytes == ""
           bytes += Utils::ByteUtils.to_u32(0)
         end
@@ -45,7 +50,9 @@ module Casper
           end
         end
         Utils::ByteUtils.hex_to_byte_array(bytes)
+        # bytes
       end
+
     end
   end
 end 
