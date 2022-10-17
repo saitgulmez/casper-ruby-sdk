629068ceaccd425fa28e1490e5de12c185076719
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2022-10-17 18:00:12 +0300

    Update CLValueSerializer

--- lib/serialization/cl_value_serializer.rb
+++ lib/serialization/cl_value_serializer.rb
@@ -18,20 +18,26 @@ require_relative '../types/cl_uref.rb'
 require_relative '../types/cl_tuple.rb'
 require_relative '../types/cl_public_key.rb'
 require_relative '../types/constants.rb'
-require_relative '../serialization/cl_value_bytes_parsers.rb'
+require_relative './cl_value_bytes_parsers.rb'
+require_relative '../utils/byte_utils.rb'
 
 # Byte serializer for CLValue
 class CLValueSerializer
+  def to_byte_array(num)
+    result = []
+    begin
+      result << (num & 0xff)
+      num >>= 8
+    end until (num == 0 || num == -1) && (result.last[7] == num[7])
+    # result.reverse
+    result
+  end
 
   def to_bytes(clvalue)
     type = clvalue.get_cl_type
     value = clvalue.get_value
     tag = CLType::TAGS[type.to_sym]
-    # puts type
-    # puts value
-    # puts CLType::TAGS[type.to_sym]
-    # puts tag
-    [1].pack("L<*").unpack1("H*")
+
     serialized = ""
     if type == "Bool"
       [1].pack("L<*").unpack1("H*") + [value.to_i].pack("C*").unpack1("H*") + [tag].pack("C*").unpack1("H*")
@@ -50,7 +56,9 @@ class CLValueSerializer
     elsif type == "U256"
       [8].pack("L<*").unpack1("H*")
     elsif type == "U512"
-      [8].pack("L<*").unpack1("H*")
+      bytes = Utils::ByteUtils.byte_array_to_hex(to_byte_array(value))[0...-2]
+      num_of_bytes = bytes.length/2
+      [num_of_bytes+1].pack("L<*").unpack1("H*") + [num_of_bytes].pack("C*").unpack1("H*") + bytes +  [tag].pack("C*").unpack1("H*")
     elsif type == "Unit"
       [0].pack("L<*").unpack1("H*") + [tag].pack("C*").unpack1("H*")
     elsif type == "String"
@@ -63,7 +71,31 @@ class CLValueSerializer
       size = clvalue.to_bytes(uref).length/2
       [size].pack("L<*").unpack1("H*") + clvalue.to_bytes(uref) + [tag].pack("C*").unpack1("H*")
     elsif type == "Option"
-      [0].pack("L<*").unpack1("H*")
+=begin
+      # Solution 1, If we choose 
+      #  CLOption.new(data, inner_type) = CLOption.new({ "cl_type": inner_type, "bytes": bytes, "parsed": parsed), "U64")
+      inner_type = value[:cl_type]
+      bytes = value[:bytes]
+      parsed = value[:parsed]
+      data = { "cl_type": inner_type, "bytes": bytes, "parsed": parsed}
+      serialize_option_cl_value(data)
+=end
+      inner_clvalue = value # => CLu64.new(1)
+      inner_value = value.get_value
+      inner_type = value.get_cl_type
+      # or
+      # inner_type = clvalue.get_inner_type
+
+      puts "inner_clvalue  #{inner_clvalue}"
+      inner_type = clvalue.get_inner_type # => or
+      inner_type = value.get_cl_type
+      puts "inner_type = #{inner_type}"
+      puts inner_value
+      bytes = Utils::ByteUtils.to_u64(inner_value)
+      puts bytes  = "01" + bytes
+      length = bytes.size/2
+      tag = CLType::TAGS[inner_type.to_sym]
+      [length].pack("L<*").unpack1("H*") + bytes + "0d" + [tag].pack("C*").unpack1("H*")
     elsif type == "List"
       [0].pack("L<*").unpack1("H*")
     elsif type == "ByteArray"
@@ -78,7 +110,6 @@ class CLValueSerializer
       value1 = clvalue1.get_value
       tag1 = CLType::TAGS[type1.to_sym]
       serialized +=  helper(clvalue.get_value[0]) + [tag].pack("C*").unpack1("H*") + [tag1].pack("C*").unpack1("H*")
-      # [18].pack("C*").unpack1("H*") + cl_type.get_data[0].to_bytes
     elsif type == "Tuple2"
       clvalue1 = clvalue.get_value[0]
       type1 = clvalue1.get_cl_type
@@ -134,7 +165,8 @@ class CLValueSerializer
     elsif type == "U8"
       [1].pack("L<*").unpack1("H*") + [value].pack("C*").unpack1("H*")
     elsif type == "U32"
-      serialized += [4].pack("L<*").unpack1("H*") + [value].pack("L<*").unpack1("H*")
+      # serialized += [4].pack("L<*").unpack1("H*") + [value].pack("L<*").unpack1("H*")
+      [4].pack("L<*").unpack1("H*") + [value].pack("L<*").unpack1("H*")
     elsif type == "U64"
       [8].pack("L<*").unpack1("H*") + [value].pack("Q<*").unpack1("H*")
     elsif type == "U128"
@@ -142,7 +174,9 @@ class CLValueSerializer
     elsif type == "U256"
       [8].pack("L<*").unpack1("H*")
     elsif type == "U512"
-      [8].pack("L<*").unpack1("H*")
+      bytes = [value].pack("Q<*").unpack1("H*")
+      bytes = bytes[0, 8]
+      [8].pack("Q<*").unpack1("H*")
     elsif type == "Unit"
       [9].pack("C*").unpack1("H*")
     elsif type == "String"
@@ -257,4 +291,27 @@ class CLValueSerializer
       "Undefined"
     end
   end
-end
+
+  def serialize_option_cl_value(data) 
+    
+=begin
+    # Solution 1
+    # puts "\nOption:"
+    cl_type = data[:cl_type]
+    bytes = data[:bytes]
+    parsed = data[:parsed]
+
+    if cl_type == "U64"
+      length = bytes.length/2
+      # puts length
+      bytes = bytes[2..]
+      value = Utils::ByteUtils.hex_to_u64_value(bytes)
+      # puts value == 1650706686882
+      clvalue = CLu64.new(value)
+      tag = CLType::TAGS[cl_type.to_sym]
+      # puts "U64: " + [length].pack("L<*").unpack1("H*") + "01" + bytes + "0d" + [tag].pack("C*").unpack1("H*")
+      [length].pack("L<*").unpack1("H*") + "01" + bytes + "0d" + [tag].pack("C*").unpack1("H*")
+    end
+=end
+  end
+end 
