914e8d28ee0d9662aa41d112a8fd28d13d01540e
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2023-04-24 23:37:02 +0300

    Implement bitwise_not method

--- lib/utils/blake2b_helper.rb
+++ lib/utils/blake2b_helper.rb
@@ -1,6 +1,23 @@
 
 class Blake2bHelper
 
-
+  def bitwise_not(n)
+    binary = n.to_s(2)
+    size = binary.size
+    if size < 32
+      binary.insert(0, "0"*(32-size))
+    end
+    updated = binary.chars.map do |ch|
+      if ch == '0'
+        ch = '1'
+      elsif ch == '1'
+        ch = '0'
+      else
+        ch = '0'
+      end
+    end
+    str = updated.join()
+    return str.to_i(2)
+  end
 
 end
