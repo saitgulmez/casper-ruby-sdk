bf2c04f04fa760f4d792601f27b26b1889c18c3f
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2024-12-04 23:30:23 +0300

    Add Constants module

---
+++ lib/utils/constants.rb
@@ -0,0 +1,27 @@
+
+module Utils
+
+  module Constants
+
+     # The size of a 32-bit number.
+     TT32 = (2 ** 32).freeze
+
+     # The size of a 256-bit number.
+     TT256 = (2 ** 256) .freeze
+
+     # The maximum possible value of an uint256.
+     UINT256_MAX = (2 ** 256).freeze
+
+     # The minimum possible value of and uint256.
+     UINT256_MIN = 0.freeze
+
+     # The maximum possible value of an int256.
+     INT256_MAX = (2 ** 256 - 1).freeze
+
+     # The minimum possible value of an int256.
+     INT256_MIN = (-2 ** 256).freeze
+
+
+  end
+
+end
