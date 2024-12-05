6f744de215a71edf49c84bcf5dc65904479c7994
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2024-12-05 23:54:06 +0300

    Add some constants

--- lib/utils/constants.rb
+++ lib/utils/constants.rb
@@ -22,6 +22,58 @@ module Utils
      INT256_MIN = (-2 ** 256).freeze
 
 
+     # The maximum possible value of an uint128.
+     UINT128_MAX = (2 ** 128).freeze
+
+     # The minimum possible value of and uint128.
+     UINT128_MIN = 0.freeze
+
+     # The maximum possible value of an int128.
+     INT128_MAX = (2 ** 128 - 1).freeze
+
+     # The minimum possible value of an int128.
+     INT128_MIN = (-2 ** 128).freeze
+
+
+     # The maximum possible value of an uint64.
+     UINT64_MAX = (2 ** 64).freeze
+
+     # The minimum possible value of and uint64.
+     UINT64_MIN = 0.freeze
+
+     # The maximum possible value of an int64.
+     INT64_MAX = (2 ** 64 - 1).freeze
+
+     # The minimum possible value of an int64.
+     INT64_MIN = (-2 ** 64).freeze
+
+
+     # The maximum possible value of an uint32.
+     UINT32_MAX = (2 ** 32).freeze
+
+     # The minimum possible value of and uint32.
+     UINT32_MIN = 0.freeze
+
+     # The maximum possible value of an int32.
+     INT32_MAX = (2 ** 32 - 1).freeze
+
+     # The minimum possible value of an int32.
+     INT32_MIN = (-2 ** 32).freeze
+
+     
+     # The maximum possible value of an uint8.
+     UINT8_MAX = (2 ** 8).freeze
+
+     # The minimum possible value of and uint8.
+     UINT8_MIN = 0.freeze
+
+     # The maximum possible value of an int8.
+     INT8_MAX = (2 ** 8 - 1).freeze
+
+     # The minimum possible value of an int8.
+     INT8_MIN = (-2 ** 8).freeze
+
+
   end
 
 end
